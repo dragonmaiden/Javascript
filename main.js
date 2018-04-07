65ea955dc632e2cf4b00e661d25317eaf5c0a573
Andy <changyuesin@gmail.com>
2018-04-07 20:28:28 +0800

    changes

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 var name = prompt("May i know what is your name?");
 var age = prompt("May i know how old are you?");
-alert("Thanks for the information " + name);
+alert("Thanks for the information " + name "please open up your webcam now");
