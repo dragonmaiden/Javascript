1da133146e076b65dec3ff7bbc8f6fd414ed2fe6
Andy <changyuesin@gmail.com>
2018-04-11 00:00:42 +0800

    edited addition game

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 var name = prompt("May i know what is your name?");
-var age = prompt("May i know how old are you?");
-alert("Thanks for the information " + name + " please open up your webcam now");
+alert("Thanks for the information " + name + " Lets play an addition game now!");
+
+var numOne = document.getElementById("num-one");
+var numTwo = document.getElementById("num-two");
+
+numOne.addEventListener("input", add);
+numTwo.addEventListener("input", add);
+
+function add(){
+	var one = parseFloat(numOne.value) || 0;
+	var two = parseFloat(numTwo.value) || 0;
+	document.getElementById("answer").value = one + two;
+}
