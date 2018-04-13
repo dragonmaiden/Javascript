c81c74b587a55bb7d6b636bf9a878d087f2acf87
Andy <changyuesin@gmail.com>
2018-04-14 01:45:20 +0800

    edited table layout

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //var name = prompt("May i know what is your name?");
 //alert("Thanks for the information " + name + " Lets play an addition game now!");
 
-var numOne = document.getElementById("num-one");
-var numTwo = document.getElementById("num-two");
+var numOne = document.getElementById("sum-one");
+var numTwo = document.getElementById("sum-two");
 
 numOne.addEventListener("input", add);
 numTwo.addEventListener("input", add);
@@ -10,5 +10,17 @@ numTwo.addEventListener("input", add);
 function add(){
 	var one = parseFloat(numOne.value) || 0;
 	var two = parseFloat(numTwo.value) || 0;
-	document.getElementById("answer").value = one + two;
+	document.getElementById("sum-answer").value = one + two;
+}
+
+var lessOne = document.getElementById("less-one");
+var lessTwo = document.getElementById("less-two");
+
+lessOne.addEventListener("input", less);
+lessTwo.addEventListener("input", less);
+
+function less(){
+	var one = parseFloat(lessOne.value) || 0;
+	var two = parseFloat(lessTwo.value) || 0;
+	document.getElementById("less-answer").value = one - two;
 }
